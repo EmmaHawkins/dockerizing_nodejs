1deeec0f0bf0841f38227aae9a22043e70ef949e
Emma Hawkins <emma.hawkins@sky.uk>
2020-08-20 14:57:11 +0100

    added css

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ const path = require('path');
  * App Variables
  */
 const app = express();
-const port = process.env.PORT || "8080";
+const port = process.env.PORT || "8000";
 /**
  *  App Configuration
  */
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Routes Definitions
