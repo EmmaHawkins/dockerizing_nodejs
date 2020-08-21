c5a436fbcb1de72353d1e4877c3a2e7685c50c2d
Emma Hawkins <emma.hawkins@sky.uk>
2020-08-21 15:57:43 +0100

    added strategy and session

--- index.js
+++ index.js
@@ -37,7 +37,26 @@ if (app.get("env") === "production") {
 /**
  * Passport Configuration
  */
-
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+  },
+  function(accessToken, refreshToken, extraparams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
 
 /**
  *  App Configuration
@@ -47,6 +66,10 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
 
 /**
  * Routes Definitions
